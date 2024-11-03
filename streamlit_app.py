c6c7820fd3502f10b527fbb5cf4ac7fb162e8afa
えんぴつ <140484956+HB-empitsu@users.noreply.github.com>
2024-11-04 00:19:39 +0900

    Update streamlit_app.py
    グラフを追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,78 @@ st.title("2024年11月2日　大雨災害")
 
 df_dam, df_river = load_data()
 
-tab1, tab2 = st.tabs(["玉川ダム", "蒼社川"])
+tab1, tab2, tab3 = st.tabs(["グラフ", "玉川ダム", "蒼社川"])
 
-tab1.subheader("玉川ダム")
+tab1.subheader("グラフ")
+
+# グラフの作成
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+fig.add_trace(
+    go.Scatter(
+        x=df_river["日時"],
+        y=df_river["中通"],
+        name="中通",
+        line=dict(color="blue"),
+    ),
+    secondary_y=False,
+)
+
+fig.add_trace(
+    go.Scatter(
+        x=df_river["日時"],
+        y=df_river["高野"],
+        name="高野",
+        line=dict(color="red"),
+    ),
+    secondary_y=False,
+)
+fig.add_trace(
+    go.Scatter(
+        x=df_river["日時"],
+        y=df_river["片山"],
+        name="片山",
+        line=dict(color="green"),
+    ),
+    secondary_y=False,
+)
+
+# 流入量
+fig.add_trace(
+    go.Scatter(
+        x=df_dam["日時"],
+        y=df_dam["全流入量"],
+        name="全流入量",
+        line=dict(color="orange"),
+    ),
+    secondary_y=True,
+)
+
+# 放流量
+fig.add_trace(
+    go.Scatter(
+        x=df_dam["日時"],
+        y=df_dam["全放流量"],
+        name="全放流量",
+        line=dict(color="purple"),
+    ),
+    secondary_y=True,
+)
+
+# レイアウトの設定
+fig.update_layout(
+    hovermode="x unified",
+    height=800,
+    hoverlabel=dict(namelength=-1),
+    xaxis=dict(tickformat="%H:%M\n%Y-%m-%d", hoverformat="%Y-%m-%d %H:%M"),
+)
+
+# グラフの表示
+tab1.plotly_chart(fig, use_container_width=True)
+
+
+tab2.subheader("玉川ダム")
+
 
 fig_dam = make_subplots(rows=2, cols=1)
 
@@ -74,13 +143,13 @@ fig_dam.update_layout(
 )
 
 # グラフの表示
-tab1.plotly_chart(fig_dam, use_container_width=True)
+tab2.plotly_chart(fig_dam, use_container_width=True)
 
 df_dam.set_index("日時", inplace=True)
 
-tab1.dataframe(df_dam.style.highlight_max(axis=0), use_container_width=True)
+tab2.dataframe(df_dam.style.highlight_max(axis=0), use_container_width=True)
 
-tab2.subheader("蒼社川")
+tab3.subheader("蒼社川")
 
 fig_river = make_subplots(rows=3, cols=1)
 
@@ -116,6 +185,7 @@ fig_river.add_trace(
     col=1,
 )
 
+
 # 水防団待機水位（高野）
 fig_river.add_hline(y=3.5, line_width=1, line_dash="dot", line_color="green", row=2, col=1)
 # 氾濫注意水位（高野）
@@ -142,8 +212,8 @@ fig_river.update_layout(
 )
 
 # グラフの表示
-tab2.plotly_chart(fig_river, use_container_width=True)
+tab3.plotly_chart(fig_river, use_container_width=True)
 
 df_river.set_index("日時", inplace=True)
 
-tab2.dataframe(df_river.style.highlight_max(axis=0), use_container_width=True)
+tab3.dataframe(df_river.style.highlight_max(axis=0), use_container_width=True)
